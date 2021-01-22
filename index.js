bfe8ebe8eb798c698131868eb6e89acfa487f800
zaidajani <zaidajani2007@gmail.com>
2021-01-22 14:57:57 +0530

    more-work

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const bcrypt = require('bcrypt');
 const db = require('crud-db');
 const port = process.env.PORT || 3000;
 const _ = require('lodash');
+const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
 
 db.initialize();
 
+function validate(user) {
+  const schema = {
+    name: Joi.string().min(3).max(45).required(),
+    password: Joi.string().min(3).required()
+  }
+
+  return Joi.validate(user, schema);
+}
+
 app.post('/user', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+ 
   let salt = await bcrypt.genSalt(10);
   const password = await bcrypt.hash(req.body.password, salt);
 
@@ -22,6 +35,9 @@ app.post('/user', async (req, res) => {
 });
 
 app.post('/auth', async (req, res) => {
+  const { error } = validate(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
   if (!db.get(req.body.name)) {
     return res.status(404).send('application not found');
   }
